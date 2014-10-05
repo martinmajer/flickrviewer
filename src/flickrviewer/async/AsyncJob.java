4bce1f8653c80bb0df81b15a46fe93a2a04dec7e
Martin Majer <martin@martinmajer.net>
2014-10-05 20:26:34 +0200

    Třídy pro asynchronní načítání dat z Flickru.

---
+++ src/flickrviewer/async/AsyncJob.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flickrviewer.async;
+
+import flickrviewer.api.FlickrException;
+
+/**
+ * Rozhraní pro callback určený k asynchronnímu načítání dat z FlickrAPI.
+ * @author Martin
+ */
+public interface AsyncJob {
+    
+    /**
+     * Načte a vrátí data z Flickr API.
+     * @return data z FlickrAPI
+     * @throws flickrviewer.api.FlickrException
+     */
+    public Object loadData() throws FlickrException;
+    
+    /**
+     * Zavolá se po dokončení přenosu.
+     * @param data data vrácená z Flickr API
+     * @param ex výjimka, v případě úspěchu null
+     */
+    public void done(Object data, FlickrException ex);
+    
+}
---
+++ src/flickrviewer/async/AsyncLoader.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flickrviewer.async;
+
+import flickrviewer.api.FlickrException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Vlákno pro asynchronní načítání dat z FlickrAPI.
+ * @author Martin
+ */
+public class AsyncLoader implements Runnable {
+    
+    private static AsyncLoader asyncLoader = null;
+    
+    private Thread loaderThread;
+    private final Object lock;
+    
+    private Queue<AsyncJob> queue;
+    
+    private AsyncLoader() {
+        loaderThread = new Thread(this);
+        loaderThread.setDaemon(true);
+        
+        lock = new Object();
+        queue = new LinkedList();
+    }
+    
+    /**
+     * Vrátí instanci objektu pro assynchronní načátání dat.
+     * @return 
+     */
+    public static AsyncLoader getInstance() {
+        if (asyncLoader == null) asyncLoader = new AsyncLoader();
+        return asyncLoader;
+    }
+    
+    public void load(AsyncJob job) {
+        synchronized (lock) {
+            if (!loaderThread.isAlive()) {
+                loaderThread.start();
+
+                // počkáme na start vlákna
+                try { lock.wait(); } 
+                catch (InterruptedException ex) {}
+            }
+            
+            queue.add(job);
+            System.out.println("AsyncLoader: Added job " + job + " (" + Thread.currentThread().getName() + ")");
+            lock.notifyAll();
+        }
+    }
+    
+
+    @Override
+    public void run() {
+        System.out.println("AsyncLoader: thread started (" + Thread.currentThread().getName() + ")");
+        
+        // oznámení o startu vlákna
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        
+        while (true) {
+            synchronized (lock) {
+                try { lock.wait(); } 
+                catch (InterruptedException ex) {}
+            }
+            
+            AsyncJob nextJob = queue.poll();
+            System.out.println("AsyncLoader: Retrieved job " + nextJob);
+            
+            try {
+                Object result = nextJob.loadData();
+                nextJob.done(result, null);
+            } 
+            catch (FlickrException ex) {
+                nextJob.done(null, ex);
+            }
+        }
+    }
+    
+    
+}
